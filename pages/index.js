4b8a9780cd4f0ea0214e50cd2034df0c9aeafc47
eightballcode <71492185+eightballcode@users.noreply.github.com>
2020-09-18 17:56:29 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 
 const Home = () => (
   <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+    <h1>ANJIM !</h1>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
+    <h3>Dhika <a href="wa.me/628994192174">Ganteng</a>!</h3>
 
     <style jsx>{`
       :global(html,body) {
